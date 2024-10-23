19310f0c3b543433e0ad86031b1193a17d912b95
immxrtlabeast <imnomakj@mail.ru>
2024-10-24 00:02:24 +0300

    Deleted unnecessary files

--- assets/constants/ROOT.js
+++
@@ -1,4 +0,0 @@
-const ROOT_CATALOG_POPULAR = document.getElementById('catalogPopular');
-const ROOT_CATALOG_COFFEE = document.getElementById('catalogCoffee');
-const ROOT_CATALOG_DRINK = document.getElementById('catalogDrink');
-const ROOT_CATALOG_FOOD = document.getElementById('catalogFood');
